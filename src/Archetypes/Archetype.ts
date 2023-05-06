6d9cd927cf9d1d9131e92b191ec34da27397f102
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-05-05 21:13:58 -0300

    Requirement 4 complete (The Archetype class was created)

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,36 @@
+import { EnergyType } from '../Energy';
+
+export interface IArchetype {
+  name: string;
+  special: number;
+  cost: number;
+  energyType: EnergyType;
+}
+
+abstract class Archetype implements IArchetype {
+  constructor(
+    private _name: string,
+    private _special: number = 0,
+    private _cost: number = 0,
+  ) { }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static get createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;

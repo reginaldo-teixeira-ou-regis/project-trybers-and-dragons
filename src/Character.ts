31af2f40a335c7560250eb9ff78a220bd2f451dc
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-05-06 00:58:06 -0300

    Requirement 8 complete (The SimpleFighter interface was created)

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -57,11 +57,11 @@ class Character implements Fighter {
     return { ...this._energy };
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
-  special(enemy: Fighter): void {
+  special(enemy: SimpleFighter): void {
     const specialStrength = this._strength * 2;
     enemy.receiveDamage(specialStrength);
   }

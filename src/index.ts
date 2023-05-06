e533023ebbc0c52a2f98b844de78ace71f860982
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-05-06 02:07:18 -0300

    Project Trybers and Dragons Finished

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,10 @@ const monster2 = new Dragon();
 const pvp = new PVP(player2, player3);
 const pve = new PVE(player1, [monster1, monster2]);
 
-function runBattles(battles: Battle[]) {
+const runBattles = (battles: Battle[]) => {
   battles.forEach((battle) => {
     battle.fight();
   });
-}
+};
 
 export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
